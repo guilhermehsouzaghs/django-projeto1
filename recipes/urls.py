16ef04220b5f9954a2315267af85a8f8b111cd96
Guilherme Henrique <guilhermehsouza.ghs@gmail.com>
2024-11-24 14:49:53 -0300

    Atualizações

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from recipes.views import home
+from recipes.views import home,recipe
 
 urlpatterns = [
     path('',home),
+    path('recipes/<int:id>/',recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -3,4 +3,9 @@ from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html',context={
+        'includes': {'teste':4,'teste2':5}
+    })
+
+def recipe(request,id):
+    return render(request, 'recipes/pages/recipe-detail.html')
